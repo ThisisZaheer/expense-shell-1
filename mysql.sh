57e8af3f7ba0903282c4c8de907b7f2b825092c7
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-07-06 20:09:51 +0530

    Shell-Script

--- mysql.sh
+++ mysql.sh
@@ -9,9 +9,9 @@ trap 'failure ${LINENO} "$BASH_COMMAND' ERR
 check_root
 
 echo "Please enter DB Password:"
-read -s mysql_root_password
+read  mysql_root_password
 
-dnf install mysql-server -y &>>LOGFILE
+dnf install mysql-serverrr -y &>>LOGFILE
 #VALIDATE $? "Installing Mysql-server"
 
 systemctl enable mysqld &>>LOGFILE
