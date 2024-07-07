704640e1fd9ceda0269350ca0d408d9c49b2d443
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-07-07 18:46:37 +0530

    Shell-Script

--- backend.sh
+++ backend.sh
@@ -3,6 +3,7 @@
 set -e
 
 # trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
 error 
 source ./common.sh
 
@@ -15,7 +16,7 @@ read  mysql_root_password
 dnf module disable nodejs -y &>>$LOGFILE
 # VALIDATE $? "Disabling default nodejs"
 
-dnf module enable nodejs:2000 -y &>>$LOGFILE
+dnf module enable nodejs:20 -y &>>$LOGFILE
 # VALIDATE $? "Enabling nodejs:20 version"
 
 dnf install nodejs -y &>>$LOGFILE
