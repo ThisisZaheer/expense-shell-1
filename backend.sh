3474ffc46c6b0b7253461ce1587c595b167d7bf6
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-07-07 18:48:54 +0530

    Shell-Script

--- backend.sh
+++ backend.sh
@@ -17,7 +17,7 @@ error
 dnf module disable nodejs -y &>>$LOGFILE
 # VALIDATE $? "Disabling default nodejs"
 
-dnf module enable nodejs:2000 -y &>>$LOGFILE
+dnf module enable nodejs:20 -y &>>$LOGFILE
 # VALIDATE $? "Enabling nodejs:20 version"
 
 dnf install nodejs -y &>>$LOGFILE
